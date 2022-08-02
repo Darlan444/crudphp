f5f8f5ed10b4c07ee1a3618fcab3282839b2fa53
Darlan444 <darlaninacio321@gmail.com>
2022-08-02 01:49:42 -0300

    HEADER-NAV

--- includes/header.php
+++ includes/header.php
@@ -1,22 +1,18 @@
-  <!-- Tell the browser to be responsive to screen width -->
-  <meta name="viewport" content="width=device-width, initial-scale=1">
-  <!-- Font Awesome -->
-  <link rel="stylesheet" href="plugins/fontawesome-free/css/all.min.css">
-  <!-- Ionicons -->
-  <link rel="stylesheet" href="https://code.ionicframework.com/ionicons/2.0.1/css/ionicons.min.css">
-  <!-- Tempusdominus Bbootstrap 4 -->
-  <link rel="stylesheet" href="plugins/tempusdominus-bootstrap-4/css/tempusdominus-bootstrap-4.min.css">
-  <!-- iCheck -->
-  <link rel="stylesheet" href="plugins/icheck-bootstrap/icheck-bootstrap.min.css">
-  <!-- JQVMap -->
-  <link rel="stylesheet" href="plugins/jqvmap/jqvmap.min.css">
-  <!-- Theme style -->
-  <link rel="stylesheet" href="dist/css/adminlte.min.css">
-  <!-- overlayScrollbars -->
-  <link rel="stylesheet" href="plugins/overlayScrollbars/css/OverlayScrollbars.min.css">
-  <!-- Daterange picker -->
-  <link rel="stylesheet" href="plugins/daterangepicker/daterangepicker.css">
-  <!-- summernote -->
-  <link rel="stylesheet" href="plugins/summernote/summernote-bs4.css">
-  <!-- Google Font: Source Sans Pro -->
-  <link href="https://fonts.googleapis.com/css?family=Source+Sans+Pro:300,400,400i,700" rel="stylesheet">
+<!DOCTYPE html>
+<html lang="PT-BR">
+<head>
+
+  <meta charset="UTF-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>REDLINE - Loja Online - CRUD</title>
+  <meta name="description" content="REDLINE é uma loja de materiais - CRUD com PHP">
+  <link rel="icon" href="./img/REDLINE-menor.png" class="icone" type="image/x-icon">
+  <link rel="stylesheet" href="./css/style.css">
+
+</head>
+
+<body>
+
+
+  
---
+++ includes/nav.php
@@ -0,0 +1,11 @@
+<?php
+include 'header.php';
+?>
+
+<header class="header">
+    <img class="logo-header" src="./img/REDLINE2.png" alt="">
+    <nav class="nav">
+        <a class="item-nav" href="#">Cadastrar Produto</a>
+        <a class="item-nav" href="#">Ver Cadastrados</a>
+    </nav>
+</header>
