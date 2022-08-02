480e97045b30bb9154e933eeb59d01f4cd871952
Darlan444 <darlaninacio321@gmail.com>
2022-08-02 02:52:07 -0300

    CARDS

--- index.php
+++ index.php
@@ -3,3 +3,32 @@
     include 'includes/header.php';
     include 'includes/nav.php';
 ?>
+
+<main class="principal">
+    <section class="section-principal">
+        <div class="produto">
+            
+        </div>
+        <div class="produto">
+
+        </div>
+        <div class="produto">
+
+        </div>
+        <div class="produto">
+
+        </div>
+        <div class="produto">
+
+        </div>
+        <div class="produto">
+
+        </div>
+        <div class="produto">
+
+        </div>
+        <div class="produto">
+
+        </div>
+    </section>
+</main>
