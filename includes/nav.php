8ff321ac2d589dddb6fb6d845a30d0825852d972
Darlan444 <darlaninacio321@gmail.com>
2022-08-09 02:33:43 -0300

    cards

--- includes/nav.php
+++ includes/nav.php
@@ -3,7 +3,9 @@ include 'header.php';
 ?>
 
 <header class="header">
-    <img class="logo-header" src="./img/REDLINE2.png" alt="">
+    <a href="./index.php">
+        <img class="logo-header" src="./img/REDLINE2.png" alt="LOGO">
+    </a>
     <nav class="nav">
         <a class="item-nav" href="#">Cadastrar Produto</a>
         <a class="item-nav" href="#">Ver Cadastrados</a>
--- index.php
+++ index.php
@@ -1,39 +1,58 @@
-<?php 
-    include 'conexao.php';
-    include 'includes/header.php';
-    include 'includes/nav.php';
+<?php
+include 'conexao.php';
+include 'includes/header.php';
+include 'includes/nav.php';
 ?>
 
 <main class="principal">
     <section class="section-principal">
+
         <div class="produto">
-            
+            <div class="img-produto">
+                <img class="img-card" src="img/REDLINE.png" alt="img-produto">
+            </div>
+            <div class="span-produto">
+                <span class="span-card">NOME PRODUTO</span>
+                <button class="btn-card" disabled="disabled">DETALHES</button>
+            </div>
         </div>
+
         <div class="produto">
+            <div class="img-produto">
+                <img class="img-card" src="img/REDLINE.png" alt="img-produto">
+            </div>
+            <div class="span-produto">
+                <span class="span-card">NOME PRODUTO</span>
+                <button class="btn-card" disabled="disabled">DETALHES</button>
+            </div>
+        </div>
 
         </div>
-        <div class="produto">
 
-        </div>
         <div class="produto">
-
+            <div class="img-produto">
+                <img class="img-card" src="img/REDLINE.png" alt="img-produto">
+            </div>
+            <div class="span-produto">
+                <span class="span-card">NOME PRODUTO</span>
+                <button class="btn-card" disabled="disabled">DETALHES</button>
+            </div>
         </div>
+
         <div class="produto">
-
+            <div class="img-produto">
+                <img class="img-card" src="img/REDLINE.png" alt="img-produto">
+            </div>
+            <div class="span-produto">
+                <span class="span-card">NOME PRODUTO</span>
+                <button class="btn-card" disabled="disabled">DETALHES</button>
+            </div>
         </div>
-        <div class="produto">
 
-        </div>
-        <div class="produto">
-
-        </div>
-        <div class="produto">
-
-        </div>
     </section>
 </main>
 
 
 <?php
-    include 'includes/footer.php';
+include 'includes/footer.php';
 ?>
