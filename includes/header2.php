1431c516ac5da18a7370272398e063d30bacc12a
Darlan444 <darlaninacio321@gmail.com>
2022-08-26 02:26:00 -0300

    keywords

--- includes/header2.php
+++ includes/header2.php
@@ -7,7 +7,7 @@
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <title>REDLINE - Loja Online - CRUD</title>
   <meta name="author" content="Darlan">
-  <meta name="keywords" content="html, css, js, php">
+  <meta name="keywords" content="loja, redline, produtos, tabela, cadastro">
   <meta name="description" content="REDLINE é uma loja de materiais - CRUD com PHP">
   <link rel="icon" href="../img/REDLINE-menor.png" class="icone" type="image/x-icon">
   <link rel="stylesheet" href="../css/style.css">
