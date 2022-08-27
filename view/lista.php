b442198e1b41e7d21db8a4c93071a9138addb863
Darlan444 <darlaninacio321@gmail.com>
2022-08-26 21:12:22 -0300

    table

--- view/lista.php
+++ view/lista.php
@@ -1,3 +1,27 @@
-<?php 
+<?php
 include '../includes/header2.php';
 include '../includes/nav2.php';
+
+?>
+
+<section class="table-section">
+
+        <table class="table">
+            <thead class="table-head">
+                <tr class="table-r">
+                    <th class="table-h">CÓDIGO</th>
+                    <th class="table-h">NOME</th>
+                    <th class="table-h">Marca</th>
+                    <th class="table-h">Valor</th>
+                    <th class="table-h">Quantidade</th>
+                </tr>
+            </thead>
+            <tbody>
+                <tr>
+                    <td>AA</td>
+                </tr>
+
+            </tbody>
+        </table>
+
+</section>
