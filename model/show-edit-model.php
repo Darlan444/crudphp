27e822bb97622e815d7ae2f5d059a5a1606d7376
Darlan444 <darlaninacio321@gmail.com>
2022-08-29 03:46:32 -0300

    show edit model

---
+++ model/show-edit-model.php
@@ -0,0 +1,51 @@
+<?php
+include '../config/conexao.php';
+
+function showInfos()
+{
+
+    $id = $_GET['id'];
+    $conn = connection();
+
+    try {
+        $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+
+        $sql = $conn->prepare("SELECT * FROM produtos WHERE id=:id");
+        $sql->bindParam(':id', $id);
+
+        $sql->execute();
+
+        $result = $sql->setFetchMode(PDO::FETCH_ASSOC);
+
+        foreach($sql->fetchAll() as $k=>$v) {
+            $nome_produto     = $v['nome_produto'];
+            $marca            = $v['marca'];
+            $valor            = $v['valor'];
+            $quantidade       = $v['quantidade'];
+            $img              = $v['img'];
+        } 
+
+        echo '  <label class="label-form" for="nome_produto">Nome do Produto</label><br>
+                <input class="input-form" name="nome_produto" id="nome_produto" type="text" spellcheck="true" value="'.$nome_produto.'" required><br>
+        
+                <label class="label-form" for="marca">Marca</label><br>
+                <input class="input-form" name="marca" id="marca" type="text" spellcheck="true" value="'.$marca.'" required><br>
+        
+                <label class="label-form" for="valor">Valor</label><br>
+                <input class="input-form" name="valor" id="valor" type="number" step="0.01" placeholder="R$ 000,00" value="'.$valor.'" required><br>
+        
+                <label class="label-form" for="quantidade">Quantidade</label><br>
+                <input class="input-form" name="quantidade" id="quantidade" type="number" min="1" value="'.$quantidade.'" required><br>
+        
+                <label class="label-form" for="img">Imagem</label><br>
+                <input class="input-form" name="img" id="img" type="text" placeholder="Link da imagem" value="'.$img.'" required><br>';
+    } catch (PDOException $e) {
+        echo "Error: " . $e->getMessage();
+    }
+    $conn = null;
+}
+
+
+?>
+
+
---
+++ view/editar.php
@@ -0,0 +1,29 @@
+<?php
+include '../includes/header2.php';
+include '../model/show-edit-model.php';
+?>
+
+
+<body class="bd-cadastro">
+
+  <?php
+    include "../includes/nav2.php";
+  ?>
+
+  <main class="form-main">
+    <section class="form-section">
+      <form class="form" method="POST" action="../model/edit-model.php">
+
+        <span class="span-form">EDITAR</span>
+
+            <?php showInfos();?>
+
+        <button class="btn-form">EDITAR</button>
+
+      </form>
+    </section>
+  </main>
+
+<?php include"../includes/footer2.php";?>  
+
+</body>
