45f709eea67ec880d1630d89e7fa4b9aa9181964
Darlan444 <darlaninacio321@gmail.com>
2022-08-28 21:03:56 -0300

    Table

--- view/lista.php
+++ view/lista.php
@@ -4,24 +4,48 @@ include '../includes/nav2.php';
 
 ?>
 
+<h1 class="h1-lista">TABELA DE PRODUTOS</h1>
+
 <section class="table-section">
 
-        <table class="table">
+    <div class="table-div">
+
+        <table class="table table-stripedr">
             <thead class="table-head">
                 <tr class="table-r">
-                    <th class="table-h">CÓDIGO</th>
-                    <th class="table-h">NOME</th>
-                    <th class="table-h">Marca</th>
-                    <th class="table-h">Valor</th>
-                    <th class="table-h">Quantidade</th>
+                    <th scope="col" class="codigo">CÓDIGO</th>
+                    <th scope="col" class="table-nome">NOME</th>
+                    <th scope="col" class="table-marca">Marca</th>
+                    <th scope="col" class="table-valor">Valor</th>
+                    <th scope="col" class="table-qtd">Quantidade</th>
+                    <th scope="col" class="table-act">Ações</th>
                 </tr>
             </thead>
             <tbody>
                 <tr>
-                    <td>AA</td>
+                    <th>1</th>
+                    <td scope="row">AA</td>
+                </tr>
+                <tr>
+                    <th>1</th>
+                    <td scope="row">AA</td>
+                </tr>
+                <tr>
+                    <th>1</th>
+                    <td scope="row">AA</td>
+                </tr>
+                <tr>
+                    <th>1</th>
+                    <td scope="row">AA</td>
+                </tr>
+                <tr>
+                    <th>1</th>
+                    <td scope="row">AA</td>
                 </tr>
 
             </tbody>
         </table>
+
+    </div>
 
 </section>
